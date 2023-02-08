5040945d07482975a3f5aeb91e67df6d44df6087
Maxim Kazachuk <dem2bb1@gmail.com>
2023-02-08 20:36:00 +0200

    Updates

---
+++ precache-manifest.994fd215fe8c95a59c35c2936b5e1c1c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99646f948341645598d8313f60569302",
+    "url": "/react-task-traffic-project-todo/index.html"
+  },
+  {
+    "revision": "e08b42827aa02cb999d6",
+    "url": "/react-task-traffic-project-todo/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "6fd17979ecff04cab9dd",
+    "url": "/react-task-traffic-project-todo/static/css/main.f49edf79.chunk.css"
+  },
+  {
+    "revision": "e08b42827aa02cb999d6",
+    "url": "/react-task-traffic-project-todo/static/js/2.fc3ef4fa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-task-traffic-project-todo/static/js/2.fc3ef4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd17979ecff04cab9dd",
+    "url": "/react-task-traffic-project-todo/static/js/main.0cc2dbeb.chunk.js"
+  },
+  {
+    "revision": "164ceebec864dd3ec86d",
+    "url": "/react-task-traffic-project-todo/static/js/runtime-main.7e501e89.js"
+  },
+  {
+    "revision": "9505b0a3d9536e440372169a4dedd9a6",
+    "url": "/react-task-traffic-project-todo/static/media/edit.9505b0a3.svg"
+  },
+  {
+    "revision": "f51bf8373ee7309d44be93c9ffe0544e",
+    "url": "/react-task-traffic-project-todo/static/media/remove.f51bf837.svg"
+  },
+  {
+    "revision": "2fc4343472898eecc14d734bd2f36e5f",
+    "url": "/react-task-traffic-project-todo/static/media/save.2fc43434.svg"
+  }
+]);
